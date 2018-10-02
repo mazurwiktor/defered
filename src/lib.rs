f0708cdbb424794143033bdc8ffd81cb375562c9
Wiktor Mazur <wiktormazur1@gmail.com>
2018-10-02 21:32:57 +0200

    Fn instead of static fn

--- src/lib.rs
+++ src/lib.rs
@@ -1,27 +1,28 @@
 use std::sync::{Arc, Mutex};
 use std::{thread, time};
 
-pub struct Defered<T> {
+pub struct Defered<T, F: Fn() -> T> {
     defered: Arc<Mutex<T>>,
-    f: fn() -> T,
+    f: F,
 }
 
-impl<T> Defered<T>
+impl<T, F> Defered<T, F>
 where
-    T: Clone + std::marker::Send + 'static,
+    T: Clone + Send + 'static,
+    F: Fn() -> T + Clone + Send + 'static 
 {
-    pub fn new(f: fn() -> T, time: time::Duration) -> Self {
+    pub fn new(f: F, time: time::Duration) -> Self {
         let defered = Arc::new(Mutex::new(f()));
         let c_mutex = defered.clone();
-        let c_time = time;
+        let c_f = f.clone();
         thread::spawn(move || loop {
             {
                 let mut lock = c_mutex.lock();
                 if let Ok(ref mut mutex) = lock {
-                    **mutex = f();
+                    **mutex = c_f();
                 }
             }
-            thread::sleep(c_time);
+            thread::sleep(time);
         });
 
         Self { defered, f }
@@ -37,13 +38,14 @@ where
 
 #[cfg(test)]
 mod tests {
+    extern crate rand;
     #[test]
     fn it_works() {
         use super::thread;
         use super::time;
         use super::Defered;
-        let x = Defered::new(|| 1, time::Duration::from_millis(10));
-        loop {
+        let x = Defered::new(|| {rand::random::<f64>()}, time::Duration::from_millis(10));
+        for _ in 0..3 {
             println!("{:?}", x.value());
             thread::sleep(time::Duration::from_millis(1000));
         }
